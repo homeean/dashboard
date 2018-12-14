1945d67585b492a65df84bdee92399e1bd00e079
stfnhmplr <stfnhmplr@gmail.com>
2018-12-14 11:17:59 +0100

    auto detect ip for local storage

--- example-script.js
+++ example-script.js
@@ -10,8 +10,13 @@ loadJSON('config.json', function(data) {
 
 function start() {
 
-    /* set default smart widgets */
-    localStorage.setItem('192.168.178.1.smartWidgets', '[{"type":10,"trackId":1,"isActive":true,"objectType":"weather"},{"type":3,"trackId":2,"isActive":true,"objectType":"smartWidget","id":4},{"type":4,"trackId":3,"isActive":true,"objectType":"smartWidget","id":5},{"type":5,"trackId":4,"isActive":true,"objectType":"smartWidget","id":6},{"type":6,"trackId":5,"isActive":true,"objectType":"smartWidget","id":7},{"type":7,"trackId":6,"isActive":true,"objectType":"smartWidget","id":8},{"type":8,"trackId":7,"isActive":true,"objectType":"smartWidget","id":9},{"type":9,"trackId":8,"isActive":true,"objectType":"smartWidget","id":10}]');
+    var ip_re = new RegExp("http:\\/\\/(.*)\\:7681", "g");
+    var homee_ip = ip_re.exec(config.homee_url)[1] || null;
+
+    if (homee_ip) {
+        /* set default smart widgets */
+        localStorage.setItem(homee_ip + '.smartWidgets', '[{"type":10,"trackId":1,"isActive":true,"objectType":"weather"},{"type":3,"trackId":2,"isActive":true,"objectType":"smartWidget","id":4},{"type":4,"trackId":3,"isActive":true,"objectType":"smartWidget","id":5},{"type":5,"trackId":4,"isActive":true,"objectType":"smartWidget","id":6},{"type":6,"trackId":5,"isActive":true,"objectType":"smartWidget","id":7},{"type":7,"trackId":6,"isActive":true,"objectType":"smartWidget","id":8},{"type":8,"trackId":7,"isActive":true,"objectType":"smartWidget","id":9},{"type":9,"trackId":8,"isActive":true,"objectType":"smartWidget","id":10}]');
+    }
 
     /* add local netatmo stream to widget list, does not work in chrome for now */
     if (config.netatmo_url) {
@@ -20,7 +25,7 @@ function start() {
             div.innerHTML = '<li style="margin: 15px"><video width="252" height="150" controls>' +
                 '<source src="'+config.netatmo_url+'" type="application/x-mpegURL"></video></li>';
             document.getElementsByClassName('widgetsWrap')[0].appendChild(div.firstChild);
-        }, 2000)
+        }, 5000)
     }
 
 }
